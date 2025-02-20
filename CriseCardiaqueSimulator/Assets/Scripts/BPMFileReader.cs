1b23d2c522a5838307e1c069f7a0c97de3297843
p.luneau <paul.luneau@mail.com>
2025-02-20 10:48:53 +0100

    Way better bpm file reader

--- CriseCardiaqueSimulator/Assets/Scripts/BPMFileReader.cs
+++ CriseCardiaqueSimulator/Assets/Scripts/BPMFileReader.cs
@@ -1,23 +1,23 @@
 using System;
 using System.IO;
 using UnityEngine;
+using UnityUtility.CustomAttributes;
 
 public class BPMFileReader : MonoBehaviour
 {
+    public int CurrentBPM => m_currentBPM;
 
     [SerializeField] private string m_bpmFilePath;
-    [SerializeField] private string m_bpmFileContent;
+    [SerializeField, Disable] private int m_currentBPM;
 
-    [NonSerialized] private FileStream m_bpmFileStream;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
     {
-        m_bpmFileContent = File.ReadAllText(m_bpmFilePath);
+        string bpmFileContent = File.ReadAllText(m_bpmFilePath);
+        if (!int.TryParse(bpmFileContent, out m_currentBPM))
+        {
+            Debug.LogError("Failed to parse the BPM file \nFile content : {bpmFileContent}");
+        }
     }
 }
