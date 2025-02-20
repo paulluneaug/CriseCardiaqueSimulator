d42cdee570184ada998b0bbbd4a27fa7208cc81a
p.luneau <paul.luneau@mail.com>
2025-02-20 17:42:13 +0100

    The bpm file reader no longer read the file each frame

--- CriseCardiaqueSimulator/Assets/Scripts/BPMFileReader.cs
+++ CriseCardiaqueSimulator/Assets/Scripts/BPMFileReader.cs
@@ -2,22 +2,30 @@ using System;
 using System.IO;
 using UnityEngine;
 using UnityUtility.CustomAttributes;
+using UnityUtility.Timer;
 
 public class BPMFileReader : MonoBehaviour
 {
     public int CurrentBPM => m_currentBPM;
 
     [SerializeField] private string m_bpmFilePath;
+    [SerializeField] private Timer m_reloadFrequency;
     [SerializeField, Disable] private int m_currentBPM;
 
-
+    private void Start()
+    {
+        m_reloadFrequency.Start();
+    }
     // Update is called once per frame
     void Update()
     {
-        string bpmFileContent = File.ReadAllText(m_bpmFilePath);
-        if (!int.TryParse(bpmFileContent, out m_currentBPM))
+        if (m_reloadFrequency.Update(Time.deltaTime))
         {
-            Debug.LogError("Failed to parse the BPM file \nFile content : {bpmFileContent}");
+            string bpmFileContent = File.ReadAllText(m_bpmFilePath);
+            if (!int.TryParse(bpmFileContent, out m_currentBPM))
+            {
+                Debug.LogError("Failed to parse the BPM file \nFile content : {bpmFileContent}");
+            }
         }
     }
 }
