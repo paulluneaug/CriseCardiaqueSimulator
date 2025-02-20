b842e3cdc4d174ab6c5d8ae6c0504cbb18edec4d
p.luneau <paul.luneau@mail.com>
2025-02-20 21:43:09 +0100

    Heart sync with bpm

--- CriseCardiaqueSimulator/Assets/Scripts/heart.cs
+++ CriseCardiaqueSimulator/Assets/Scripts/heart.cs
@@ -4,27 +4,32 @@ using UnityEngine.Serialization;
 
 public class Heart : MonoBehaviour
 {
-    [SerializeField, FormerlySerializedAs("curve")] private AnimationCurve m_curve;
+    [SerializeField] private AnimationCurve m_curve;
     [SerializeField] private Renderer m_renderer;
+    [SerializeField] private BPMFileReader m_bpmReader;
 
     [SerializeField] private string m_propertyName = "_heartbeat";
 
     [NonSerialized] private MaterialPropertyBlock m_materialPropertyBlock;
     [NonSerialized] private int m_propertyID;
 
+    [NonSerialized] private float m_time;
+
     void Start()
     {
         m_materialPropertyBlock = new MaterialPropertyBlock();
         m_propertyID = Shader.PropertyToID(m_propertyName);
+
+        m_time = 0.0f;
     }
 
     void Update()
     {
-        //transform.localScale = new Vector3(transform.localScale.x, curve.Evaluate(Time.time), transform.localScale.z);
+        m_time += Time.deltaTime * ((float)m_bpmReader.CurrentBPM / 60.0f);
 
         m_renderer.GetPropertyBlock(m_materialPropertyBlock);
 
-        m_materialPropertyBlock.SetFloat(m_propertyID, m_curve.Evaluate(Time.time));
+        m_materialPropertyBlock.SetFloat(m_propertyID, m_curve.Evaluate(m_time));
 
         m_renderer.SetPropertyBlock(m_materialPropertyBlock);
 
