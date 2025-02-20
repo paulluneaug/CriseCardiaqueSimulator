58ae1b4b41866cd8474674577f8c99c627fb1545
p.luneau <paul.luneau@mail.com>
2025-02-20 10:44:36 +0100

    Cleaned the heart shader controller

--- CriseCardiaqueSimulator/Assets/Scripts/heart.cs
+++ CriseCardiaqueSimulator/Assets/Scripts/heart.cs
@@ -1,31 +1,32 @@
+using System;
 using UnityEngine;
-using System.Collections;
+using UnityEngine.Serialization;
 
 public class Heart : MonoBehaviour
 {
-    public AnimationCurve curve;
-    Renderer rend;
-    [SerializeField] Material material;
+    [SerializeField, FormerlySerializedAs("curve")] private AnimationCurve m_curve;
+    [SerializeField] private Renderer m_renderer;
+
+    [SerializeField] private string m_propertyName = "_heartbeat";
+
+    [NonSerialized] private MaterialPropertyBlock m_materialPropertyBlock;
+    [NonSerialized] private int m_propertyID;
 
     void Start()
     {
-        if (material == null)
-        {
-            Debug.LogWarning("NULL");
-        }
-        else
-        {
-
-            //rend = GetComponent<Renderer>();
-            //rend.material.shader = material;
-        }
+        m_materialPropertyBlock = new MaterialPropertyBlock();
+        m_propertyID = Shader.PropertyToID(m_propertyName);
     }
 
     void Update()
     {
         //transform.localScale = new Vector3(transform.localScale.x, curve.Evaluate(Time.time), transform.localScale.z);
 
-        material = GetComponent<Renderer>().material;
-        material.SetFloat("_heartbeat", curve.Evaluate(Time.time));
+        m_renderer.GetPropertyBlock(m_materialPropertyBlock);
+
+        m_materialPropertyBlock.SetFloat(m_propertyID, m_curve.Evaluate(Time.time));
+
+        m_renderer.SetPropertyBlock(m_materialPropertyBlock);
+
     }
 }
